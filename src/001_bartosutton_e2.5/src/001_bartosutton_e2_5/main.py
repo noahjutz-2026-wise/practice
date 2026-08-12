b6ccb191c6c37aa669bfc62e20b610b4a3b37c6f
Noah Jutz <noah@noahjutz.com>
2026-08-12 17:32:30 +0200

    Partially implement e2.5

--- src/001_bartosutton_e2.5/src/001_bartosutton_e2_5/main.py
+++ src/001_bartosutton_e2.5/src/001_bartosutton_e2_5/main.py
@@ -1,4 +1,28 @@
 import matplotlib
 import numpy as np
 
-print("helo")
+action_values = np.zeros(10)
+
+q = np.ones(10)
+
+epsilon = 0.1
+
+alpha = 0.9
+
+
+def reward(a):
+    return np.random.normal(q[a], 1)
+
+
+for i in range(10_000):
+    is_explore = np.random.random() < epsilon
+    if is_explore:
+        a = np.random.randint(0, 10)
+    else:
+        a = np.argmax(action_values)
+
+    r = reward(a)
+
+    # todo compute action value
+    # todo walk q (rewards)
+    # todo log values for plotting
