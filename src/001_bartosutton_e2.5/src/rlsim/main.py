d11f276fab7edb5be69f78ccdb559b7047c1b724
Noah Jutz <noah@noahjutz.com>
2026-08-13 10:26:42 +0200

    Run simulation 1000 times

--- src/001_bartosutton_e2.5/src/rlsim/main.py
+++ src/001_bartosutton_e2.5/src/rlsim/main.py
@@ -1,14 +1,26 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from numpy.typing import NDArray
 
 from rlsim.simulation import Simulation
 
 
+def sample_average(n: int):
+    n += 1  # 0-indexing to 1-indexing
+    return 1 / n
+
+
+def runs(simulation: Simulation, n_runs: int) -> NDArray[np.float64]:
+    average = np.zeros(simulation.n_steps)
+    for i in range(n_runs):
+        average += simulation.run()
+        print(i)
+    average /= n_runs
+    return average
+
+
 def main():
-    simulation = Simulation(10)
-    epsilon = 0.1
-    alpha = 0.1
-    n_steps = 10_000
-    result = simulation.run(epsilon, lambda n: alpha, n_steps)
-    print(result)
-    plt.plot(result)
+    sim = Simulation(n_actions=10, epsilon=0.1, alpha=sample_average, n_steps=10_000)
+    data = runs(sim, 1_000)
+    plt.plot(data)
     plt.show()
--- src/001_bartosutton_e2.5/src/rlsim/simulation.py
+++ src/001_bartosutton_e2.5/src/rlsim/simulation.py
@@ -5,9 +5,18 @@ from numpy.typing import NDArray
 
 
 class Simulation:
-    def __init__(self, n_actions: int) -> None:
+    def __init__(
+        self,
+        n_actions: int,
+        epsilon: float,
+        alpha: Callable[[int], float],
+        n_steps: int,
+    ) -> None:
         self.Q_star = np.ones(n_actions)  # rewards
         self.Q = np.zeros(n_actions)  # action value estimates
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.n_steps = n_steps
 
     def observe_reward(self, a: int):
         return np.random.normal(self.Q_star[a], 1)
@@ -15,13 +24,11 @@ class Simulation:
     def estimate_next_value(self, action, reward, alpha):
         return self.Q[action] + alpha * (reward - self.Q[action])
 
-    def run(
-        self, epsilon: float, alpha: Callable[[int], float], n_steps: int
-    ) -> NDArray[np.float64]:
-        rewards = np.zeros(n_steps)
-        for i in range(n_steps):
-            alpha_n = alpha(i)
-            is_explore = np.random.random() < epsilon
+    def run(self) -> NDArray[np.float64]:
+        rewards = np.zeros(self.n_steps)
+        for i in range(self.n_steps):
+            alpha_n = self.alpha(i)
+            is_explore = np.random.random() < self.epsilon
             if is_explore:
                 a = np.random.randint(0, 10)
             else:
