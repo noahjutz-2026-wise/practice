4ca79504e5ad081c6c90142515141eb2d8b21fa3
Noah Jutz <noah@noahjutz.com>
2026-08-13 09:38:42 +0200

    Wrap simulation in class

--- src/001_bartosutton_e2.5/src/001_bartosutton_e2_5/main.py
+++ src/001_bartosutton_e2.5/src/001_bartosutton_e2_5/main.py
@@ -1,45 +1,46 @@
-from typing import Callable
+from collections.abc import Callable
 
-import matplotlib
 import numpy as np
 from numpy.typing import NDArray
 
-Q_star = np.ones(10)  # rewards
-Q = np.zeros(10)  # action value estimates
 
+class Simulation:
+    def __init__(self, n_actions: int) -> None:
+        self.Q_star = np.ones(n_actions)  # rewards
+        self.Q = np.zeros(n_actions)  # action value estimates
+
+    def observe_reward(self, a: int):
+        return np.random.normal(self.Q_star[a], 1)
+
+    def estimate_next_value(self, action, reward, alpha):
+        return self.Q[action] + alpha * (reward - self.Q[action])
+
+    def run(
+        self, epsilon: float, alpha: Callable[[int], float], n_steps: int
+    ) -> NDArray[np.float64]:
+        rewards = np.array(n_steps)
+        for i in range(n_steps):
+            alpha_n = alpha(i)
+            is_explore = np.random.random() < epsilon
+            if is_explore:
+                a = np.random.randint(0, 10)
+            else:
+                a = np.argmax(self.Q)
+
+            r = self.observe_reward(a)
+
+            self.Q[a] = self.estimate_next_value(a, r, alpha_n)
+
+            # simulate nonstationary problem
+            self.Q_star += np.random.normal(0, 0.1, 10)
+            print(self.Q_star)
+
+            # todo log values for plotting
+        return rewards
+
+
+simulation = Simulation(10)
 epsilon = 0.1
 alpha = 0.9
-
-
-def observe_reward(a):
-    return np.random.normal(Q_star[a], 1)
-
-
-def estimate_next_value(action, reward, alpha):
-    return Q[action] + alpha * (reward - Q[action])
-
-
-def run(
-    epsilon: float, alpha: Callable[[int], float], n_steps: int
-) -> NDArray[np.float64]:
-    rewards = np.array(n_steps)
-    for i in range(n_steps):
-        alpha_n = alpha(i)
-        is_explore = np.random.random() < epsilon
-        if is_explore:
-            a = np.random.randint(0, 10)
-        else:
-            a = np.argmax(Q)
-
-        r = observe_reward(a)
-
-        Q[a] = estimate_next_value(a, r, alpha_n)
-
-        Q_star += np.random.normal(0, 0.1, 10)  # simulate nonstationary problem
-        print(Q_star)
-
-        # todo log values for plotting
-    return rewards
-
-
-run(epsilon, lambda n: alpha, 100)
+n_steps = 100
+simulation.run(epsilon, lambda n: alpha, n_steps)
