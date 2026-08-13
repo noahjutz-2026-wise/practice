4b264e51ffae51359d501714a7258202201a84d1
Noah Jutz <noah@noahjutz.com>
2026-08-13 09:00:59 +0200

    Improve notation, estimate weighted average value

--- src/001_bartosutton_e2.5/src/001_bartosutton_e2_5/main.py
+++ src/001_bartosutton_e2.5/src/001_bartosutton_e2_5/main.py
@@ -1,28 +1,32 @@
 import matplotlib
 import numpy as np
 
-action_values = np.zeros(10)
-
-q = np.ones(10)
+Q_star = np.ones(10)  # rewards
+Q = np.zeros(10)  # action value estimates
 
 epsilon = 0.1
-
 alpha = 0.9
 
 
-def reward(a):
-    return np.random.normal(q[a], 1)
+def observe_reward(a):
+    return np.random.normal(Q_star[a], 1)
+
+
+def estimate_next_value(action, reward, alpha):
+    return Q[action] + alpha * (reward - Q[action])
 
 
 for i in range(10_000):
+    alpha_n = alpha  # constant
     is_explore = np.random.random() < epsilon
     if is_explore:
         a = np.random.randint(0, 10)
     else:
-        a = np.argmax(action_values)
+        a = np.argmax(Q)
 
-    r = reward(a)
+    r = observe_reward(a)
 
-    # todo compute action value
+    Q[a] = estimate_next_value(a, r, alpha_n)
+
     # todo walk q (rewards)
     # todo log values for plotting
