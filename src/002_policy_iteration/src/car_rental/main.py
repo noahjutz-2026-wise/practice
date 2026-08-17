097575f61c7d1867796d09c671ae2a71bd42b293
Noah Jutz <noah@noahjutz.com>
2026-08-17 17:43:16 +0200

    Extract code into expected_return fn

--- src/002_policy_iteration/src/car_rental/main.py
+++ src/002_policy_iteration/src/car_rental/main.py
@@ -19,53 +19,60 @@ pi = np.zeros((21, 21), dtype=np.int8)
 v = np.zeros((21, 21))
 
 
+def expected_return(s1: int, s2: int) -> float:
+    a = pi[s1, s2]
+    n1 = s1 - a
+    n2 = s2 + a
+
+    n_moved = abs(a)
+    n_out_1 = scipy.stats.poisson.expect(
+        lambda n: np.minimum(n1, n), args=(lambda_out[0],)
+    )
+    n_out_2 = scipy.stats.poisson.expect(
+        lambda n: np.minimum(n2, n), args=(lambda_out[1],)
+    )
+
+    value = 0.0
+    p_s1_ = [
+        sum(
+            scipy.stats.poisson.pmf(i, mu=lambda_in[0])
+            * scipy.stats.poisson.pmf(j, mu=lambda_out[0])
+            for i in range(12)
+            for j in range(12)
+            if s1_ == min(20, n1 - min(n1, j) + i)
+        )
+        for s1_ in range(21)
+    ]
+    p_s2_ = [
+        sum(
+            scipy.stats.poisson.pmf(i, mu=lambda_in[1])
+            * scipy.stats.poisson.pmf(j, mu=lambda_out[1])
+            for i in range(12)
+            for j in range(12)
+            if s2_ == min(20, n2 - min(n2, j) + i)
+        )
+        for s2_ in range(21)
+    ]
+
+    for s1_ in range(21):
+        for s2_ in range(21):
+            value += p_s1_[s1_] * p_s2_[s2_] * v[s1_, s2_]
+
+    return -2 * n_moved + 10 * (n_out_1 + n_out_2) + gamma * value
+
+
 # 2. Policy Evaluation
 def eval(pi: NDArray[np.int8], v: NDArray[np.float64], theta: float = 0.1):
     for k in itertools.count(1):
         delta = 0
         for s1, s2 in np.ndindex(v.shape):
+            s1 = int(s1)
+            s2 = int(s2)
             v_old = v[s1, s2]
-            a = pi[s1, s2]
-            n1 = s1 - a
-            n2 = s2 + a
-
-            n_moved = abs(a)
-            n_out_1 = scipy.stats.poisson.expect(
-                lambda n: np.minimum(n1, n), args=(lambda_out[0],)
-            )
-            n_out_2 = scipy.stats.poisson.expect(
-                lambda n: np.minimum(n2, n), args=(lambda_out[1],)
-            )
-
-            value = 0.0
-            p_s1_ = [
-                sum(
-                    scipy.stats.poisson.pmf(i, mu=lambda_in[0])
-                    * scipy.stats.poisson.pmf(j, mu=lambda_out[0])
-                    for i in range(12)
-                    for j in range(12)
-                    if s1_ == min(20, n1 - min(n1, j) + i)
-                )
-                for s1_ in range(21)
-            ]
-            p_s2_ = [
-                sum(
-                    scipy.stats.poisson.pmf(i, mu=lambda_in[1])
-                    * scipy.stats.poisson.pmf(j, mu=lambda_out[1])
-                    for i in range(12)
-                    for j in range(12)
-                    if s2_ == min(20, n2 - min(n2, j) + i)
-                )
-                for s2_ in range(21)
-            ]
-
-            for s1_ in range(21):
-                for s2_ in range(21):
-                    value += p_s1_[s1_] * p_s2_[s2_] * v[s1_, s2_]
-
-            v[s1, s2] = -2 * n_moved + 10 * (n_out_1 + n_out_2) + gamma * value
+            v[s1, s2] = expected_return(s1, s2)
             delta = max(delta, abs(v_old - v[s1, s2]))
             print(f"(k={k}, delta={delta}) ({s1},{s2}):{v[s1, s2]}")
+
         if delta < theta:
             print(v)
             break
