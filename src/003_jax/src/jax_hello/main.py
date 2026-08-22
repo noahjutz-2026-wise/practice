fd0b4be478f2a07c9f84589d950f1e03a5257f08
Noah Jutz <noah@noahjutz.com>
2026-08-22 13:53:48 +0200

    JAX Hello world

--- src/003_jax/src/jax_hello/main.py
+++ src/003_jax/src/jax_hello/main.py
@@ -1,2 +1,10 @@
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+
+
 def main():
-    print("hi")
+    x_jnp = jnp.linspace(0, 10, 1000)
+    y_jnp = 2 * jnp.sin(x_jnp) * jnp.cos(x_jnp)
+    print(x_jnp.devices())
+    plt.plot(x_jnp, y_jnp)
+    plt.show()
