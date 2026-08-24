e55d9b940358880c24dde0f42aa7d3894b79c551
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-24 15:34:09 +0200

    Rename project

---
+++ src/005_hello_hafner_libs/src/hello_hafner/main.py
@@ -0,0 +1,27 @@
+def server():
+    import portal
+
+    server = portal.Server(2222)
+    server.bind("add", lambda x, y: x + y)
+    server.bind("greet", lambda msg: print("Message from client:", msg))
+    server.start()
+
+
+def client():
+    import portal
+
+    client = portal.Client("localhost:2222")
+    future = client.add(12, 42)
+    result = future.result()
+    print(result)  # 54
+    client.greet("Hello World")
+
+
+def main():
+    import portal
+
+    server_proc = portal.Process(server, start=True)
+    client_proc = portal.Process(client, start=True)
+    client_proc.join()
+    server_proc.kill()
+    print("Done")
