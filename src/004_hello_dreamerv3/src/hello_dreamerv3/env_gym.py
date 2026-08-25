9638333a4e8fbafaf13dc4b4bde8a50665171ac5
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-25 14:56:37 +0200

    Add env_gym and FromGymnasium converter
    FromGymnasium by https://gist.github.com/qxcv/e8641342c102c2aa714c9caeca724101

---
+++ src/004_hello_dreamerv3/src/hello_dreamerv3/env_gym.py
@@ -0,0 +1,33 @@
+from typing import SupportsFloat
+
+import gymnasium as gym
+import numpy as np
+from typing_extensions import override
+
+
+class MyGymEnv(gym.Env[np.ndarray, int]):
+    def __init__(self):
+        self.observation_space = gym.spaces.Box(low=0, high=200)
+        self.action_space = gym.spaces.Discrete(11)
+
+        self._state: int = 0
+
+    @override
+    def step(
+        self, action: int
+    ) -> tuple[float, SupportsFloat, bool, bool, dict[str, str]]:
+
+        reward: int = action - 1
+        self._state += reward
+        terminated: bool = self._state >= 200
+        truncated = False
+
+        observation = self._get_obs()
+
+        return observation, reward, terminated, truncated, {}
+
+    def _get_obs(self) -> float:
+        return self._state
+
+    def reset(self, *, seed: int | None = None, options: dict | None = None):
+        self._state = 0
