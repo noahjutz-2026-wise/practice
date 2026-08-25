a934ec0e0f97c8c439d9f4b8766e061528a71c4e
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-25 08:15:40 +0200

    Run 200 episodes

--- src/006_gymnasium/src/hello_gymnasium/main.py
+++ src/006_gymnasium/src/hello_gymnasium/main.py
@@ -2,18 +2,18 @@ import gymnasium as gym
 
 env = gym.make("CartPole-v1", render_mode="human")
 
-observation, info = env.reset()
 
-episode_over = False
-total_reward = 0
+for i in range(200):
+    total_reward = 0
+    observation, info = env.reset()
+    while True:
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+        total_reward += float(reward)
+        if truncated or terminated:
+            break
+    print(total_reward)
 
-while not episode_over:
-    action = env.action_space.sample()
-    observation, reward, terminated, truncated, info = env.step(action)
-    total_reward += float(reward)
-    episode_over = truncated or terminated
-
-print(total_reward)
 env.close()
 
 
