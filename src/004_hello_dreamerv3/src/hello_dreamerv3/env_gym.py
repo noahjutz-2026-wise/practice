a3125b7ec1b2ed8f313f3e7d6ff3aa83e21b197a
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-25 16:20:35 +0200

    Fix env_gym mismatch between obs space and _get_obs

--- src/004_hello_dreamerv3/src/hello_dreamerv3/env_gym.py
+++ src/004_hello_dreamerv3/src/hello_dreamerv3/env_gym.py
@@ -2,12 +2,13 @@ from typing import SupportsFloat
 
 import gymnasium as gym
 import numpy as np
+from gymnasium.core import ObsType
 from typing_extensions import override
 
 
 class MyGymEnv(gym.Env[np.ndarray, int]):
     def __init__(self):
-        self.observation_space = gym.spaces.Box(low=0, high=200)
+        self.observation_space = gym.spaces.Box(low=0, high=200, shape=(1,))
         self.action_space = gym.spaces.Discrete(11)
 
         self.render_mode = "rgb_array"
@@ -20,7 +21,7 @@ class MyGymEnv(gym.Env[np.ndarray, int]):
     ) -> tuple[float, SupportsFloat, bool, bool, dict[str, str]]:
 
         reward: int = action - 1
-        self._state += reward
+        self._state = max(0, min(200, self._state + reward))
         terminated: bool = self._state >= 200
         truncated = False
 
@@ -28,8 +29,11 @@ class MyGymEnv(gym.Env[np.ndarray, int]):
 
         return observation, reward, terminated, truncated, {}
 
-    def _get_obs(self) -> float:
-        return self._state
+    def _get_obs(self) -> tuple[float]:
+        return (self._state,)
 
-    def reset(self, *, seed: int | None = None, options: dict | None = None):
+    def reset(
+        self, *, seed: int | None = None, options: dict | None = None
+    ) -> tuple[ObsType, dict]:
         self._state = 0
+        return self.observation_space.sample(), {}
