e1b135d195cf86bbc5703f4bb5ca7aecc522dd55
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-26 10:53:30 +0200

    Create bins

--- src/007_monte_carlo/src/gym_app/main.py
+++ src/007_monte_carlo/src/gym_app/main.py
@@ -1,6 +1,18 @@
 import gymnasium as gym
+import numpy as np
 
 env = gym.make("CartPole-v1", render_mode="human")
+
+q = np.zeros(shape=(100, 100, 100, 100), dtype=np.uint8)
+
+bins = np.vstack(
+    (
+        np.linspace(-4.8, 4.8, num=100),
+        np.linspace(-5, 5, num=100),
+        np.linspace(-0.418, 0.418, num=100),
+        np.linspace(-5, 5, num=100),
+    )
+)
 
 observation, info = env.reset()
 total_reward = 0
@@ -8,6 +20,7 @@ total_reward = 0
 while True:
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
+    print(observation)
     total_reward += reward
     if truncated or terminated:
         break
