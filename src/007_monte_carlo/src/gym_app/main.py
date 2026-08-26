b4cc81c2cc51b413ec61178fbb69868f910b7e1b
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-26 17:19:10 +0200

    Count state visits N

--- src/007_monte_carlo/src/gym_app/main.py
+++ src/007_monte_carlo/src/gym_app/main.py
@@ -13,8 +13,8 @@ plt.show(block=False)
 
 env = gym.make("CartPole-v1", render_mode=None)
 
-q = np.zeros(shape=(100,) * 4, dtype=np.uint8)
-visited = np.zeros(shape=(100,) * 4, dtype=np.bool)
+q = np.zeros(shape=(100,) * 4, dtype=np.float64)
+N = np.zeros(shape=(100,) * 4, dtype=np.uint8)
 
 bins = np.vstack(
     (
@@ -32,12 +32,12 @@ def bin(observation: NDArray[np.float64]) -> NDArray[np.uint8]:
     return (observation[:, None] >= bins).sum(axis=1)
 
 
-def resolve(bin: NDArray[np.uint8]):
-    return bins[np.arange(4), bin]
+def resolve(bin: NDArray[np.uint8]) -> NDArray[np.float64]:
+    return bins[np.arange(4), bin]  # todo out of bounds exception
 
 
 for episode in itertools.count():
-    visited.fill(False)
+    N.fill(False)
     observation, info = env.reset()
     o_b = bin(observation)
     G = 0
@@ -46,12 +46,12 @@ for episode in itertools.count():
         observation, reward, terminated, truncated, info = env.step(action)
         o_b = bin(observation)
         G = G * gamma + reward
+        N[tuple(o_b)] += 1
 
         # prediction
-        if not visited[tuple(o_b)]:
-            visited[tuple(o_b)] = True
+        if N[tuple(o_b)] == 1:
             v = q[tuple(o_b)]
-            q[tuple(o_b)] = (1 / (episode + 1)) * v + (v - G)
+            q[tuple(o_b)] = (1 / N[tuple(o_b)]) * v + (v - G)
 
         if truncated or terminated:
             break
