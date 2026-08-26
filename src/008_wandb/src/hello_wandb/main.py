42085d4bc3129030f5237a908f7b9681a1d617b9
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-26 12:13:00 +0200

    Add wandb quickstart

--- src/008_wandb/src/hello_wandb/main.py
+++ src/008_wandb/src/hello_wandb/main.py
@@ -1,2 +1,11 @@
+import wandb
+
+project = "my-awesome-proj"
+
+with wandb.init(project=project) as run:
+    for i in range(1000):
+        run.log({"accuracy": i, "loss": -float(i) / 10})
+
+
 def main():
     pass
