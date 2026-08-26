ea2d86108fae3b0f27376f7034cdcc70c1fe3846
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-26 16:15:50 +0200

    Cleanup

--- src/007_monte_carlo/src/gym_app/main.py
+++ src/007_monte_carlo/src/gym_app/main.py
@@ -1,14 +1,10 @@
+import itertools
+
 import gymnasium as gym
 import numpy as np
+from numpy.typing import NDArray
 
-from . import control
-
-
-def discretize(observation):
-    o_binned = (observation[:, None] >= bins).sum(axis=1)
-    o_discrete = bins[np.arange(4), o_binned]
-    return (o_binned, o_discrete)
-
+from . import control, prediction
 
 env = gym.make("CartPole-v1", render_mode="human")
 
@@ -23,17 +19,31 @@ bins = np.vstack(
     )
 )
 
-observation, info = env.reset()
-o_b, o_d = discretize(observation)
-total_reward = 0
+gamma = 0.9
 
-while True:
-    action = control.action(env, o_d)
-    observation, reward, terminated, truncated, info = env.step(action)
-    # state = observation
-    total_reward += reward
-    if truncated or terminated:
-        break
+
+def bin(observation: NDArray[np.float64]) -> NDArray[np.uint8]:
+    return (observation[:, None] >= bins).sum(axis=1)
+
+
+def resolve(bin: NDArray[np.uint8]):
+    return bins[np.arange(4), bin]
+
+
+for episode in range(1000):
+    observation, info = env.reset()
+    o_b = bin(observation)
+    total_reward = 0
+    for step in itertools.count():
+        action = control.action(env, resolve(o_b))
+        observation, reward, terminated, truncated, info = env.step(action)
+        o_b = bin(observation)
+        total_reward += reward
+        if truncated or terminated:
+            break
+
+    prediction.evaluate(q, ...)
+
 
 env.close()
 
