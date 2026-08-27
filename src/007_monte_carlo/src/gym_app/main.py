f7c9ed56a57e36703128a60a9f8e1744486e671d
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-27 14:14:59 +0200

    Add wandb

--- src/007_monte_carlo/src/gym_app/main.py
+++ src/007_monte_carlo/src/gym_app/main.py
@@ -1,93 +1,15 @@
-import itertools
+import wandb
 
-import gymnasium as gym
-import matplotlib.pyplot as plt
-import numpy as np
-import pyinstrument
-from numpy.typing import NDArray
-
-from . import control, estimation, viz
-
-config = {"n_bins": (15, 15, 15, 15), "gamma": 0.9, "epsilon": 0.01}
-n_bins = config["n_bins"]
-gamma = config["gamma"]
-epsilon = config["epsilon"]
-
-fig, ax = plt.subplots()
-(ln0,) = ax.plot([], [], label="action 0 (left)")
-(ln1,) = ax.plot([], [], label="action 1 (right)")
-ax.legend()
-plt.show(block=False)
-
-env = gym.make("CartPole-v1", render_mode=None)
-
-
-Q = np.zeros(shape=n_bins + (2,), dtype=np.float64)
-M = np.zeros(
-    shape=n_bins + (2,), dtype=np.uint32
-)  # Monte Carlo incremental Average (+ 1/M * error)
-
-bin_lo = np.array([-4.8, -5.0, -0.418, -5.0])
-bin_hi = np.array([4.8, 5.0, 0.418, 5.0])
-n_bins_arr = np.array(n_bins, dtype=np.float64)
-bin_step = (bin_hi - bin_lo) / (n_bins_arr - 1)
-bin_max = np.array(n_bins, dtype=np.int64)  # max index = n_bins (one past last edge)
-
-
-def bin(observation: NDArray[np.float64]) -> NDArray[np.int64]:
-    return np.clip(
-        ((observation - bin_lo) / bin_step + 0.5).astype(np.int64), 0, bin_max
-    )
-
-
-def resolve(b: NDArray[np.int64]) -> NDArray[np.float64]:
-    return bin_lo + b * bin_step
-
-
-p = pyinstrument.Profiler()
-p.start()
-for episode in range(10000):
-    if episode == 50000:
-        env = gym.make("CartPole-v1", render_mode="human")
-    rewards = []
-    states = []
-    actions = []
-    observation, info = env.reset()
-    o_b = bin(observation)
-    for step in itertools.count():
-        states.append(o_b)
-        action = control.action(o_b, Q, epsilon)
-        observation, reward, terminated, truncated, info = env.step(action)
-        o_b = bin(observation)
-
-        rewards.append(reward)
-        actions.append(action)
-
-        if truncated or terminated:
-            break
-
-    states = np.array(states)
-    rewards = np.array(rewards)
-    actions = np.array(actions)
-    M, V = estimation.monte_carlo(rewards, states, actions, gamma, M, Q)
-
-    if episode % 1000 == 0:
-        print(f"ep {episode}")
-    if episode % 20000 == 0:
-        y = viz.value_by_angle(Q, M)
-        x = np.arange(y.shape[0])
-        ln0.set_data(x, y[:, 0])
-        ln1.set_data(x, y[:, 1])
-        ax.relim()
-        ax.autoscale_view()
-        plt.pause(0.01)
-
-p.stop()
-p.print()
-p.open_in_browser()
-
-env.close()
+from . import train
 
 
 def main():
-    pass
+    config = {
+        "n_bins": (15, 15, 15, 15),
+        "gamma": 0.9,
+        "epsilon": 0.01,
+        "episodes": 2000,
+    }
+
+    with wandb.init(entity="jno", project="monte_carlo", config=config) as run:
+        train.train(run)
---
+++ src/007_monte_carlo/src/gym_app/train.py
@@ -0,0 +1,87 @@
+import itertools
+
+import gymnasium as gym
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.typing import NDArray
+
+import wandb
+
+from . import control, estimation, viz
+
+
+def train(run: wandb.Run):
+    n_bins = run.config["n_bins"]
+    gamma = run.config["gamma"]
+    epsilon = run.config["epsilon"]
+    episodes = run.config["episodes"]
+
+    fig, ax = plt.subplots()
+    (ln0,) = ax.plot([], [], label="action 0 (left)")
+    (ln1,) = ax.plot([], [], label="action 1 (right)")
+    ax.legend()
+    plt.show(block=False)
+
+    env = gym.make("CartPole-v1", render_mode=None)
+
+    Q = np.zeros(shape=n_bins + (2,), dtype=np.float64)
+    M = np.zeros(
+        shape=n_bins + (2,), dtype=np.uint32
+    )  # Monte Carlo incremental Average (+ 1/M * error)
+
+    bin_lo = np.array([-4.8, -5.0, -0.418, -5.0])
+    bin_hi = np.array([4.8, 5.0, 0.418, 5.0])
+    n_bins_arr = np.array(n_bins, dtype=np.float64)
+    bin_step = (bin_hi - bin_lo) / (n_bins_arr - 1)
+    bin_max = np.array(
+        n_bins, dtype=np.int64
+    )  # max index = n_bins (one past last edge)
+
+    def bin(observation: NDArray[np.float64]) -> NDArray[np.int64]:
+        return np.clip(
+            ((observation - bin_lo) / bin_step + 0.5).astype(np.int64), 0, bin_max
+        )
+
+    def resolve(b: NDArray[np.int64]) -> NDArray[np.float64]:
+        return bin_lo + b * bin_step
+
+    for episode in range(episodes):
+        if episode == 50000:
+            env = gym.make("CartPole-v1", render_mode="human")
+        rewards = []
+        states = []
+        actions = []
+        observation, info = env.reset()
+        o_b = bin(observation)
+        for step in itertools.count():
+            states.append(o_b)
+            action = control.action(o_b, Q, epsilon)
+            observation, reward, terminated, truncated, info = env.step(action)
+            o_b = bin(observation)
+
+            rewards.append(reward)
+            actions.append(action)
+
+            if truncated or terminated:
+                break
+
+        states = np.array(states)
+        rewards = np.array(rewards)
+        actions = np.array(actions)
+        M, Q = estimation.monte_carlo(rewards, states, actions, gamma, M, Q)
+
+        if episode % 1000 == 0:
+            print(f"ep {episode}")
+        if episode % 20000 == 0:
+            y = viz.value_by_angle(Q, M)
+            x = np.arange(y.shape[0])
+            ln0.set_data(x, y[:, 0])
+            ln1.set_data(x, y[:, 1])
+            ax.relim()
+            ax.autoscale_view()
+            plt.pause(0.01)
+
+    env.close()
+
+    def main():
+        pass
