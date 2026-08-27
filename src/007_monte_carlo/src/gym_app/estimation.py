34100aab1b5be66c90ef813c07b0b4f6e9900269
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-27 11:10:02 +0200

    Fix: check if first-visit of action-value pair

--- src/007_monte_carlo/src/gym_app/estimation.py
+++ src/007_monte_carlo/src/gym_app/estimation.py
@@ -18,7 +18,9 @@ def monte_carlo(
     G = 0
     for t in range(len(rewards) - 1, -1, -1):
         G = gamma * G + rewards[t]
-        if not (states[t] == states[:t]).all(axis=1).any():
+        state_mask = (states[t] == states[:t]).all(axis=1)
+        action_mask = actions[t] == actions[:t]
+        if not (state_mask & action_mask).any():
             idx = tuple(states[t]) + (actions[t],)
             M[idx] += 1
             Q[idx] += 1 / M[idx] * (G - Q[idx])
