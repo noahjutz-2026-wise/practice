baba5bf8785f02a92ab22b2761385a9d9580d594
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-28 13:06:02 +0200

    Run 50 tests with different epsilon

--- src/007_monte_carlo/src/gym_app/main.py
+++ src/007_monte_carlo/src/gym_app/main.py
@@ -9,26 +9,28 @@ def main():
         "n_bins": (15, 15, 15, 15),
         "gamma": 0.9,
         "epsilon": 0.01,
-        "episodes": 1000,
+        "episodes": 2000,
         "log_every": 100,
     }
 
-    n_runs = 10
+    n_runs = 50
 
-    for r in range(n_runs):
-        print(f"Run {r}")
-        with wandb.init(
-            mode="disabled",
-            entity="tjno",
-            project="monte_carlo",
-            config=config,
-            group="offpolicy_1",
-        ) as run:
-            run.define_metric("episode")
-            run.define_metric("cum_reward", step_metric="episode", summary="max")
-            run.define_metric("steps", step_metric="episode", summary="max")
-            run.define_metric("q_coverage", step_metric="episode", summary="max")
-            run.define_metric("q_value", step_metric="episode", summary="mean")
-            run.define_metric("stability", step_metric="episode")
+    for epsilon in (0, 0.01, 0.1, 0.5, 1):
+        config["epsilon"] = epsilon
+        for r in range(n_runs):
+            print(f"Epsilon={epsilon}; Run={r}")
+            with wandb.init(
+                mode="online",
+                entity="tjno",
+                project="monte_carlo",
+                config=config,
+                group="offpolicy_3",
+            ) as run:
+                run.define_metric("episode")
+                run.define_metric("cum_reward", step_metric="episode", summary="max")
+                run.define_metric("steps", step_metric="episode", summary="max")
+                run.define_metric("q_coverage", step_metric="episode", summary="max")
+                run.define_metric("q_value", step_metric="episode", summary="mean")
+                run.define_metric("stability", step_metric="episode")
 
-            Q = train.train(run)
+                Q = train.train(run)
