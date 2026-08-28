16c52f5a85f25db4465504aed27b315dd217a2ad
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-28 11:39:46 +0200

    Run experiment n_runs times

--- src/007_monte_carlo/src/gym_app/main.py
+++ src/007_monte_carlo/src/gym_app/main.py
@@ -13,14 +13,21 @@ def main():
         "log_every": 100,
     }
 
-    with wandb.init(
-        entity="tjno", project="monte_carlo", config=config, mode="disabled"
-    ) as run:
-        run.define_metric("episode")
-        run.define_metric("cum_reward", step_metric="episode", summary="max")
-        run.define_metric("steps", step_metric="episode", summary="max")
-        run.define_metric("q_coverage", step_metric="episode", summary="max")
-        run.define_metric("q_value", step_metric="episode", summary="mean")
-        run.define_metric("stability", step_metric="episode")
+    n_runs = 50
 
-        Q = train.train(run)
+    for episode in range(n_runs):
+        with wandb.init(
+            mode="disabled",
+            entity="tjno",
+            project="monte_carlo",
+            config=config,
+            group="offpolicy_1",
+        ) as run:
+            run.define_metric("episode")
+            run.define_metric("cum_reward", step_metric="episode", summary="max")
+            run.define_metric("steps", step_metric="episode", summary="max")
+            run.define_metric("q_coverage", step_metric="episode", summary="max")
+            run.define_metric("q_value", step_metric="episode", summary="mean")
+            run.define_metric("stability", step_metric="episode")
+
+            Q = train.train(run)
