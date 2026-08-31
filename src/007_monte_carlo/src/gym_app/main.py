f184a22526ca398b4515c1b40588b5003fd93a8e
Noah Jutz <git@noahjutz.anonaddy.com>
2026-08-31 13:55:14 +0200

    Run double Q-learning experiments

--- src/007_monte_carlo/src/gym_app/main.py
+++ src/007_monte_carlo/src/gym_app/main.py
@@ -7,7 +7,7 @@ from . import train
 entity = "tjno"
 project = "rl"
 mode = "online"
-group = "compare_mc_ql_sarsa_expected_sarsa_1"
+group = "double_q_learning_batch1"
 
 
 def experiment_eval(config: wandb.sdk.Config, model_name: str | None = None):
@@ -22,37 +22,34 @@ def experiment_eval(config: wandb.sdk.Config, model_name: str | None = None):
 
 def experiment_train(config: wandb.sdk.Config, model_name: str | None = None):
     config["task"] = "train"
-    n_runs = 500
+    n_runs = 20
 
     for r in range(n_runs):
         print(f"Run={r}")
-        for method in ["expected_sarsa", "sarsa", "q_learning", "monte_carlo"]:
-            print(f"Run {r} method {method}")
-            config["prediction_method"] = method
-            with wandb.init(
-                mode=mode,
-                entity=entity,
-                project=project,
-                config=config,
-                group=group,
-            ) as run:
-                run.define_metric("episode")
-                run.define_metric("cum_reward", step_metric="episode", summary="max")
-                run.define_metric("steps", step_metric="episode", summary="max")
-                run.define_metric("q_coverage", step_metric="episode", summary="max")
-                run.define_metric("q_value", step_metric="episode", summary="mean")
-                run.define_metric("stability", step_metric="episode")
+        with wandb.init(
+            mode=mode,
+            entity=entity,
+            project=project,
+            config=config,
+            group=group,
+        ) as run:
+            run.define_metric("episode")
+            run.define_metric("cum_reward", step_metric="episode", summary="max")
+            run.define_metric("steps", step_metric="episode", summary="max")
+            run.define_metric("q_coverage", step_metric="episode", summary="max")
+            run.define_metric("q_value", step_metric="episode", summary="mean")
+            run.define_metric("stability", step_metric="episode")
 
-                Q = train.train(run)
+            Q = train.train(run)
 
-                if model_name is not None:
-                    np.savez_compressed("model.npz", Q)
-                    run.log_model(path="model.npz", name=model_name)
+            if model_name is not None:
+                np.savez_compressed("model.npz", Q)
+                run.log_model(path="model.npz", name=model_name)
 
 
 def main():
     config = {
-        "prediction_method": "expected_sarsa",
+        "prediction_method": "double_q_learning",
         "n_bins": (15, 15, 15, 15),
         "gamma": 0.9,  # Discount factor
         "epsilon": 0.1,  # Exploration rate
