76c9157aa334e9ec2f502f24c0ae1f0b5fc81bc2
Noah Jutz <git@noahjutz.anonaddy.com>
2026-09-03 13:43:57 +0200

    Experiment with tensordict

--- src/011_pytorch/src/hello_pytorch/main.py
+++ src/011_pytorch/src/hello_pytorch/main.py
@@ -1,22 +1,14 @@
 import torch
-import torch.nn as nn
-from torchvision.models import ResNet18_Weights, resnet18
-
-
-class Net(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
+from tensordict import TensorDict
 
 
 def main():
-    model = resnet18(weights=ResNet18_Weights.DEFAULT)
-    data = torch.rand(1, 3, 64, 64)
-    labels = torch.rand(1, 1000)
-
-    prediction = model(data)  # forward pass
-
-    loss = (prediction - labels).sum()
-    loss.backward()
-
-    optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
-    optim.step()
+    td = TensorDict(
+        {
+            "z": torch.zeros((10, 10)),
+            "o": torch.ones((10, 10, 2)),
+            "r": torch.rand((10, 10, 2, 20)),
+        },
+        batch_size=[10, 10],
+    )
+    print(td[..., :2])
