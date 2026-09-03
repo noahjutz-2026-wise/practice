49656c03ce7496b83b729f1774e1e7d96c22c3e0
Noah Jutz <git@noahjutz.anonaddy.com>
2026-09-03 16:01:00 +0200

    Add NN policy

--- src/010_torchrl/src/hello_torchrl/main.py
+++ src/010_torchrl/src/hello_torchrl/main.py
@@ -1,8 +1,14 @@
+import torch
+from tensordict.nn import TensorDictModule
 from torchrl.envs import GymEnv, StepCounter, TransformedEnv, step_mdp
 
 
 def main():
     env = GymEnv("Pendulum-v1")
     env = TransformedEnv(env, StepCounter(max_steps=10))
-    data = env.rollout(max_steps=100)
-    print(data["next"]["truncated"])
+
+    module = torch.nn.LazyLinear(out_features=env.action_spec.shape[-1])
+    policy = TensorDictModule(module, in_keys=["observation"], out_keys=["action"])
+
+    rollout = env.rollout(max_steps=100, policy=policy)
+    print(rollout)
