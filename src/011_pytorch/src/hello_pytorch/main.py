012d97820c7946315f7f86873d9c2c120dd3d4c2
Noah Jutz <git@noahjutz.anonaddy.com>
2026-09-03 13:25:14 +0200

    bump

--- src/011_pytorch/src/hello_pytorch/main.py
+++ src/011_pytorch/src/hello_pytorch/main.py
@@ -1,6 +1,22 @@
 import torch
+import torch.nn as nn
+from torchvision.models import ResNet18_Weights, resnet18
+
+
+class Net(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
 
 
 def main():
-    d = torch.tensor([[1, 2], [3, 4]])
-    print(d)
+    model = resnet18(weights=ResNet18_Weights.DEFAULT)
+    data = torch.rand(1, 3, 64, 64)
+    labels = torch.rand(1, 1000)
+
+    prediction = model(data)  # forward pass
+
+    loss = (prediction - labels).sum()
+    loss.backward()
+
+    optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
+    optim.step()
