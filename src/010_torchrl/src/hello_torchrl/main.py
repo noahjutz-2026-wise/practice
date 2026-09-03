39aa07369985e702aaf0cae6add41aa6c707067b
Noah Jutz <git@noahjutz.anonaddy.com>
2026-09-03 18:09:25 +0200

    Implement flappy birt w dreamer in ray (copy from tutorial)

--- src/010_torchrl/src/hello_torchrl/main.py
+++ src/010_torchrl/src/hello_torchrl/main.py
@@ -1,38 +1,49 @@
-from pprint import pprint
+from pathlib import Path
 
-from ray.rllib.algorithms.ppo import PPOConfig
+import ray
+from ray import tune
+from ray.rllib.algorithms.dreamerv3.dreamerv3 import DreamerV3Config
 
-# Create a config instance for the PPO algorithm.
-config = PPOConfig().environment("Pendulum-v1")
 
-config.env_runners(num_env_runners=2)
+def _env_creator(ctx):
+    import flappy_bird_gymnasium  # doctest: +SKIP
+    import gymnasium as gym
+    from ray.rllib.env.wrappers.atari_wrappers import NormalizedImageEnv
+    from supersuit.generic_wrappers import resize_v1
 
-config.training(
-    lr=0.0002,
-    train_batch_size_per_learner=2000,
-    num_epochs=10,
-)
-
-ppo = config.build_algo()
-for _ in range(4):
-    pprint(ppo.train())
-
-checkpoint_path = ppo.save_to_path()
-
-print("eval")
-
-config.evaluation(
-    evaluation_interval=1,
-    evaluation_num_env_runners=2,
-    evaluation_duration_unit="episodes",
-    evaluation_duration=10,
-)
-
-ppo_with_evaluation = config.build_algo()
-
-for _ in range(3):
-    pprint(ppo_with_evaluation.train())
+    return NormalizedImageEnv(
+        resize_v1(  # resize to 64x64 and normalize images
+            gym.make("FlappyBird-rgb-v0", audio_on=False), x_size=64, y_size=64
+        )
+    )
 
 
 def main():
-    pass
+    ray_tmp = Path.home() / ".ray_tmp"
+    ray_tmp.mkdir(parents=True, exist_ok=True)
+    ray.init(_temp_dir=str(ray_tmp))
+
+    # Register the FlappyBird-rgb-v0 env including necessary wrappers via the
+    # `tune.register_env()` API.
+    tune.register_env("flappy-bird", _env_creator)
+
+    # Define the `config` variable to use for training.
+    config = (
+        DreamerV3Config()
+        # set the env to the pre-registered string
+        .environment("flappy-bird")
+        # play around with the insanely high number of hyperparameters for DreamerV3 ;)
+        .training(
+            model_size="S",
+            training_ratio=1024,
+        )
+    )
+
+    # Run the tuner job.
+    results = tune.Tuner(trainable="DreamerV3", param_space=config).fit()
+    return results
+
+
+if __name__ == "__main__":
+    main()
+
