e16f0d24b26ed6bdbef9d5bd79d32e142f20bad2
Noah Jutz <git@noahjutz.anonaddy.com>
2026-09-12 16:12:31 +0200

    Add training data

--- src/011_pytorch/src/hello_pytorch/main.py
+++ src/011_pytorch/src/hello_pytorch/main.py
@@ -1,14 +1,18 @@
+import matplotlib.pyplot as plt
 import torch
-from tensordict import TensorDict
+from torch import nn
 
 
 def main():
-    td = TensorDict(
-        {
-            "z": torch.zeros((10, 10)),
-            "o": torch.ones((10, 10, 2)),
-            "r": torch.rand((10, 10, 2, 20)),
-        },
-        batch_size=[10, 10],
-    )
-    print(td[..., :2])
+    weight = 0.7
+    bias = 0.3
+
+    start = 0
+    end = 1
+    step = 0.02
+    X = torch.arange(start, end, step).unsqueeze(dim=1)
+    y = weight * X + bias
+
+    train_split = int(0.8 * len(X))
+    X_train, y_train = X[:train_split], y[:train_split]
+    X_test, y_test = X[train_split:], y[train_split:]
