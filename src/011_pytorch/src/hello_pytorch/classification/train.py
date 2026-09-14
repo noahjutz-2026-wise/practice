dcb8587c17e42095b7304b5d4d5b07eea5991876
Noah Jutz <git@noahjutz.anonaddy.com>
2026-09-14 17:10:23 +0200

    Create CircleModelV0

--- src/011_pytorch/src/hello_pytorch/classification/train.py
+++ src/011_pytorch/src/hello_pytorch/classification/train.py
@@ -1,6 +1,19 @@
 import torch
 from sklearn.datasets import make_circles
 from sklearn.model_selection import train_test_split
+from torch import nn
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class CircleModelV0(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features=2, out_features=5)
+        self.layer_2 = nn.Linear(in_features=5, out_features=1)
+
+    def forward(self, x):
+        return self.layer_2(self.layer_1(x))
 
 
 def main():
@@ -13,3 +26,6 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
+
+    mod_0 = CircleModelV0().to(device)
+    print(mod_0)
